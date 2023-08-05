d310f4e5d5b01e211bee569a14dc0544ad30340e
Daris02 <hei.raymond.2@gmail.com>
2023-08-05 09:10:16 +0300

    feat: update render table

--- Dashboard/assets/js/scriptAllbooking.js
+++ Dashboard/assets/js/scriptAllbooking.js
@@ -4,11 +4,20 @@ const URL = "http://localhost:8000";
 
 const tbody = $("#tbody");
 
-async function displayAllBooking(bookings) {
+function displayAllBooking(bookings) {
   let tab = ``;
 
-  await bookings.forEach((booking) => {
-      tab += `
+  bookings.forEach((booking) => {
+    let status =
+      '<div class="actions"> <a href="#" class="btn btn-sm bg-success-light mr-2">Arrived</a> </div>';
+    if (booking.is_cancelled == false) {
+      status =
+        '<div class="actions"> <a href="#" class="btn btn-sm bg-warning-light mr-2">Pending</a> </div>';
+    } else if (booking.is_cancelled == true) {
+      status =
+        '<div class="actions"> <a href="#" class="btn btn-sm bg-danger-light mr-2">Cancelled</a> </div>';
+    }
+      tbody.append(`
         <tr>
           <td>BKG-${booking.id}</td>
           <td>
@@ -24,18 +33,31 @@ async function displayAllBooking(bookings) {
           <td>${booking.email}</td>
           <td>${booking.principal_contact}</td>
           <td>
-            <div class="actions"> <a href="#" class="btn btn-sm bg-success-light mr-2">Active</a> </div>
+            ${status}
           </td>
           <td class="text-right">
             <div class="dropdown dropdown-action"> <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-ellipsis-v ellipse_color"></i></a>
-              <div class="dropdown-menu dropdown-menu-right"> <a class="dropdown-item" href="edit-booking.html"><i class="fas fa-pencil-alt m-r-5"></i> Edit</a> <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_asset"><i class="fas fa-trash-alt m-r-5"></i> Delete</a> </div>
+              <div class="dropdown-menu dropdown-menu-right"> 
+                <a class="dropdown-item" href="edit-booking.html"><i class="fas fa-pencil-alt m-r-5"></i> Edit</a>
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_asset_${booking.id}"><i class="fas fa-trash-alt m-r-5"></i> Delete</a>
+              </div>
+            </div>
+            <div id="delete_asset_${booking.id}" class="modal fade delete-modal" role="dialog">
+              <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-body text-center"> <img src="../assets/img/sent.png" alt="" width="50" height="46">
+                      <h3 class="delete_class">Are you sure want to delete this Asset?</h3>
+                      <div class="m-t-20"> <a href="#" class="btn btn-white" data-dismiss="modal">Close</a>
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                      </div>
+                    </div>
+                </div>
+              </div>
             </div>
           </td>
         </tr>
-        `;
+        `);
     })
-
-    tbody.innerHTML = tab;
     $(document).ready(function () {
       $('#myTable').DataTable();
     })
@@ -44,8 +66,5 @@ async function displayAllBooking(bookings) {
 fetch(`${URL}/ReservationsWithCustomerInfo`)
   .then((res) => res.json())
   .then((data) => {
-    console.log('====================================');
-    console.log(data);
-    console.log('====================================');
     displayAllBooking(data);
   });
