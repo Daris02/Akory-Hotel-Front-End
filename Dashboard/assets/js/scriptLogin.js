af22782638467245a6542ceb5a249de0bb5a3b22
Daris02 <hei.raymond.2@gmail.com>
2023-08-08 08:53:47 +0300

    fix: update login data

--- Dashboard/assets/js/scriptLogin.js
+++ Dashboard/assets/js/scriptLogin.js
@@ -1,10 +1,12 @@
-const URL = `http://localhost:8000`;
-
 let status;
 
 const loginForm = document.getElementById("loginForm");
 const welcomeMessageElement = document.getElementById("welcomingMessage");
 const user_profile = document.getElementById("user_profile");
+const messageElement = document.getElementById("message");
+
+let welcome;
+let user;
 
 loginForm.addEventListener("submit", async (event) => {
     event.preventDefault();
@@ -13,7 +15,7 @@ loginForm.addEventListener("submit", async (event) => {
     const password = document.getElementById("password").value;
 
     try {
-        const response = await fetch(`${URL}/login`, {
+        const response = await fetch(`http://localhost:8000/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -27,14 +29,22 @@ loginForm.addEventListener("submit", async (event) => {
         const data = await response.json();
 
         if (response.ok) {
+            welcome = `Welcome ${data.first_name} ${data.last_name} !`;
+            user = `${data.first_name} ${data.last_name}`;
             window.location.replace('../index.html');
-            welcomeMessageElement.innerText = `Welcome ${data.first_name} ${data.last_name} !`;
-            user_profile.innerText = `${data.first_name} ${data.last_name}`;
+        } else {
+            messageElement.textContent = "Authification Failed!";
+            setTimeout(() => {
+                messageElement.textContent = null;
+            }, 1500);
         }
     } catch (error) {
         console.error(error);
     }
 });
 
-// "john.doe@gmail.com"
-// "password1"
+// welcomeMessageElement.innerText = welcome;
+// user_profile.innerText = user;
+
+console.log(welcome);
+console.log(welcome);
