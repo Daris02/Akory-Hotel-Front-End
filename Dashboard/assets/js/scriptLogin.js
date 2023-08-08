133f7f64fe86b84455b15606c09d5b36dc06d4c4
Daris02 <hei.raymond.2@gmail.com>
2023-08-08 08:22:12 +0300

    fix: finish login

--- Dashboard/assets/js/scriptLogin.js
+++ Dashboard/assets/js/scriptLogin.js
@@ -2,34 +2,39 @@ const URL = `http://localhost:8000`;
 
 let status;
 
-function login() {
-    let email = document.getElementById(`user_email`).value;
-    let pwd = document.getElementById(`password`).value;
+const loginForm = document.getElementById("loginForm");
+const welcomeMessageElement = document.getElementById("welcomingMessage");
+const user_profile = document.getElementById("user_profile");
 
-    fetch(`${URL}/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            user_email: email,
-            password: pwd
-        }),
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        if (data.error) {
-            alert('KO 👎')
-        } else if(data.token) {
-            const welcomeMessageElement = document.getElementById("welcomingMessage");
-            const user_profile = document.getElementById("user_profile");
+loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const user_email = document.getElementById("user_email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        const response = await fetch(`${URL}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                user_email: user_email,
+                password: password
+            }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            window.location.replace('../index.html');
             welcomeMessageElement.innerText = `Welcome ${data.first_name} ${data.last_name} !`;
             user_profile.innerText = `${data.first_name} ${data.last_name}`;
-            alert('OK 👍')
-            window.location.replace('../index.html');
         }
-    });
-}
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // "john.doe@gmail.com"
 // "password1"
