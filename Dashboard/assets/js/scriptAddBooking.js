11ad97a0120d564bb6d9f5d89cc22c7983c996e8
Daris02 <hei.raymond.2@gmail.com>
2023-08-08 10:22:10 +0300

    fix: remove fecthing for test

--- Dashboard/assets/js/scriptAddBooking.js
+++ Dashboard/assets/js/scriptAddBooking.js
@@ -6,31 +6,24 @@ addBooking.addEventListener("submit", async (event) => {
     const user_email = document.getElementById("user_email").value;
     const password = document.getElementById("password").value;
 
-    try {
-        const response = await fetch(`http://localhost:8000/login`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                user_email: user_email,
-                password: password
-            }),
-        });
-
-        const data = await response.json();
-
-        if (response.ok) {
-            welcome = `Welcome ${data.first_name} ${data.last_name} !`;
-            user = `${data.first_name} ${data.last_name}`;
-            window.location.replace('../index.html');
-        } else {
-            messageElement.textContent = "Authification Failed!";
-            setTimeout(() => {
-                messageElement.textContent = null;
-            }, 1500);
-        }
-    } catch (error) {
-        console.error(error);
-    }
+    // try {
+    //     const response = await fetch(`http://localhost:8000/reservation`, {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify({
+    //                 date_arrived: "2023-10-01 13:30:00",
+    //                 leaving_date: "2023-10-02 06:00:00",
+    //                 number_of_person: 3,
+    //                 is_cancelled: false,
+    //                 id_customer: 34,
+    //                 id_room: 111
+    //         }),
+    //     });
+    //     const data = await response.json();
+    //     console.log(data);
+    // } catch (error) {
+    //     console.error(error);
+    // }
 });
